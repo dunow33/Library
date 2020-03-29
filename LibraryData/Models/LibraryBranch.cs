f9f3d73504f80e441f9a811e5863c31a3b552aee
Brian Donnoe <dunow33@hotmail.com>
2020-03-28 21:03:54 -0400

    adding objects to build the library system

---
+++ LibraryData/Models/LibraryBranch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class LibraryBranch
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Telephone { get; set; }
+        public string Description { get; set; }
+        public DateTime OpenDate { get; set; }
+
+        public virtual IEnumerable<Patron> Patrons { get; set; }
+        public virtual IEnumberable<LibraryAsset> LibraryAssests { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
